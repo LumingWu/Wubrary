e1180d15d59afd2f4b8057c5f4d27dfa7e449c7e
LuLu <LuLu@LuLu-PC>
2015-01-02 18:58:02 -0500

    Added SmartButton(Label)

---
+++ src/FX/SmartButton.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FX;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ *
+ * @author LuLu
+ */
+public class SmartButton extends Label {
+    private ImageView _static;
+    private ImageView _react;
+    
+    private int state;
+    public SmartButton() {
+        
+    }
+
+    public void setPressedNode(ImageView node) {
+        _react = node;
+    }
+
+    public void setReleasedNode(ImageView node) {
+        _static = node;
+        setGraphic(_react);
+        state = 1;
+    }
+    
+    public void flip(){
+        switch(state){
+            case 1:{
+                setGraphic(_react);
+                state = 2;
+                break;
+            }
+            case 2:{
+                setGraphic(_static);
+                state = 1;
+                break;
+            }
+        }
+    }
+    
+}
