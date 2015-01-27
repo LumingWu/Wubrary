699e44df31c382275d529358a13b8d2f437c1457
Luming <Luming@LuLu>
2015-01-26 22:29:30 -0500

    SmartButton is improved.

--- src/FX/SmartButton.java
+++ src/FX/SmartButton.java
@@ -7,43 +7,49 @@ package FX;
 
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.AudioClip;
 
 /**
  *
  * @author LuLu
  */
 public class SmartButton extends Label {
-    private ImageView _static;
-    private ImageView _react;
-    
-    private int state;
-    public SmartButton() {
-        
-    }
+    private ImageView _press;
+    private ImageView _release;
+    private AudioClip _presssound;
+    private AudioClip _releasesound;
 
     public void setPressedNode(ImageView node) {
-        _react = node;
+        _press = node;
     }
 
     public void setReleasedNode(ImageView node) {
-        _static = node;
-        setGraphic(_static);
-        state = 1;
+        _release = node;
     }
     
-    public void flip(){
+    public void setPressedSound(AudioClip presssound){
+        _presssound = presssound;
+    }
+    
+    public void setReleaseSound(AudioClip releasesound){
+        _releasesound = releasesound;
+    }
+    
+    public void switchState(ButtonState state){
         switch(state){
-            case 1:{
-                setGraphic(_react);
-                state = 2;
+            case PRESSED:{
+                setGraphic(_press);
+                _presssound.play();
                 break;
             }
-            case 2:{
-                setGraphic(_static);
-                state = 1;
+            case RELEASED:{
+                setGraphic(_release);
+                _releasesound.play();
                 break;
             }
         }
     }
-    
+    public enum ButtonState{
+        PRESSED, RELEASED
+    }
 }
